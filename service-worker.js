d605665fc0057b15b58dcd0624b8b166e172afc3
Jessica60250 <g6kachou@hotmail.fr>
2026-08-14 15:42:10 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,61 @@
+const CACHE_NAME = "jeu-alicia-v1";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./indexn.html",
+    "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(FILES_TO_CACHE))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            )
+        )
+    );
+
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+
+    if (event.request.method !== "GET") return;
+
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+
+            return fetch(event.request).then(response => {
+
+                if (
+                    response &&
+                    response.status === 200 &&
+                    response.type === "basic"
+                ) {
+                    const responseClone = response.clone();
+
+                    caches.open(CACHE_NAME).then(cache => {
+                        cache.put(event.request, responseClone);
+                    });
+                }
+
+                return response;
+            });
+
+        })
+    );
+});
